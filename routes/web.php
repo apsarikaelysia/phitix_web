1893061fa59fa68c4a89096e14998c64fb7bc5c7
lanaibnufauzi <117898671+apsarikaelysia@users.noreply.github.com>
2023-05-25 11:51:38 +0700

    update routes url page

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,47 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.login');
+});
+
+Route::get('/login', function () {
+    return view('admin.login');
+});
+
+Route::get('/index', function () {
+    return view('admin.pages.index');
+});
+
+Route::get('/dataayam', function () {
+    return view('admin.pages.dataayam');
+});
+
+Route::get('/datapakan', function () {
+    return view('admin.pages.datapakan');
+});
+
+Route::get('/dataovk', function () {
+    return view('admin.pages.dataovk');
+});
+
+Route::get('/datatenagakerja', function () {
+    return view('admin.pages.datatenagakerja');
+});
+
+Route::get('/datadistribusi', function () {
+    return view('admin.pages.datadistribusi');
+});
+
+Route::get('/datapengeluaran', function () {
+    return view('admin.pages.datapengeluaran');
+});
+
+Route::get('/datapendapatan', function () {
+    return view('admin.pages.datapendapatan');
 });
