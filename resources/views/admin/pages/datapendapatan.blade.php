311cfd8810d6f468c04db32a69595744d18b68aa
lanaibnufauzi <117898671+apsarikaelysia@users.noreply.github.com>
2023-05-25 11:50:36 +0700

    add templating blade

---
+++ resources/views/admin/pages/datapendapatan.blade.php
@@ -0,0 +1,178 @@
+@extends('admin.layout.main')
+
+@section('title', 'Data Pendapatan')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <h2 class="mb-2 page-title">Data Pendapatan</h2>
+                <p class="card-text">DataTables is a plug-in for the jQuery Javascript library. It is a highly flexible tool,
+                    built upon the foundations of progressive enhancement, that adds all of these advanced features to any
+                    HTML table. </p>
+                <div class="row my-4">
+                    <!-- Small table -->
+                    <div class="col-md-12">
+                        <div class="card shadow">
+                            <div class="card-body">
+                                <!-- table -->
+                                <table class="table datatables" id="dataTable-1">
+                                    <div class="align-right text-right mb-3">
+                                        <button class="btn btn-success btn-sm" data-toggle="modal"
+                                            data-target="#addModal">Add</button>
+                                    </div>
+
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Id</th>
+                                            <th>Tanggal Periode</th>
+                                            <th>Pemasukan</th>
+                                            <th>Pengeluaran</th>
+                                            <th>Pendapatan</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>p</td>
+                                            <td>p</td>
+                                            <td>p</td>
+                                            <td>p</td>
+                                            <td>p</td>
+                                            <td>
+
+                                                <button class="btn btn-warning btn-sm" data-toggle="modal"
+                                                    data-target="#editModal">Edit</button>
+
+                                                <button class="btn btn-danger btn-sm" data-toggle="modal"
+                                                    data-target="#deleteModal">Delete</button>
+
+                                            </td>
+                                        </tr>
+
+                                        <!-- Delete Modal -->
+                                        <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog"
+                                            aria-labelledby="defaultModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog" role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="defaultModalLabel">Delete Modal</h5>
+                                                        <button type="button" class="close" data-dismiss="modal"
+                                                            aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        Yakin Ingin Menghapus Data?
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn mb-2 btn-success"
+                                                            data-dismiss="modal">Close</button>
+                                                        <button type="button" class="btn mb-2 btn-danger">Delete</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <!-- Edit Modal -->
+                                        <div class="modal fade" id="editModal" tabindex="-1" role="dialog"
+                                            aria-labelledby="defaultModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog" role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="defaultModalLabel">Edit Modal</h5>
+                                                        <button type="button" class="close" data-dismiss="modal"
+                                                            aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <form>
+                                                            <div class="form-group">
+                                                                <label for="recipient-name"
+                                                                    class="col-form-label">Label</label>
+                                                                <input type="text" class="form-control"
+                                                                    id="recipient-name">
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <label for="recipient-name"
+                                                                    class="col-form-label">Date</label>
+                                                                <input type="date" class="form-control"
+                                                                    id="recipient-name">
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <label for="message-text"
+                                                                    class="col-form-label">Label</label>
+                                                                <textarea class="form-control" id="message-text"></textarea>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn mb-2 btn-danger"
+                                                            data-dismiss="modal">Close</button>
+                                                        <button type="button" class="btn mb-2 btn-success">Save
+                                                            changes</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </tbody>
+                                </table>
+                                <!-- Add Modal -->
+                                <div class="modal fade" id="addModal" tabindex="-1" role="dialog"
+                                    aria-labelledby="defaultModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="defaultModalLabel">Add Modal</h5>
+                                                <button type="button" class="close" data-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form>
+                                                    <div class="form-group">
+                                                        <label for="recipient-name" class="col-form-label">Label</label>
+                                                        <input type="text" class="form-control" id="recipient-name">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="recipient-name" class="col-form-label">Date</label>
+                                                        <input type="date" class="form-control" id="recipient-name">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="message-text" class="col-form-label">Label</label>
+                                                        <textarea class="form-control" id="message-text"></textarea>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn mb-2 btn-danger"
+                                                    data-dismiss="modal">Close</button>
+                                                <button type="button" class="btn mb-2 btn-success">Save changes</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div> <!-- simple table -->
+                </div> <!-- end section -->
+            </div> <!-- .col-12 -->
+        </div> <!-- .row -->
+    </div> <!-- .container-fluid -->
+@endsection
+
+@section('script')
+    <script>
+        $('#dataTable-1').DataTable({
+            autoWidth: true,
+            "lengthMenu": [
+                [16, 32, 64, -1],
+                [16, 32, 64, "All"]
+            ]
+        });
+    </script>
+@endsection
---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,71 @@
+<aside class="sidebar-left border-right bg-white shadow" id="leftSidebar" data-simplebar>
+    <a href="#" class="btn collapseSidebar toggle-btn d-lg-none text-muted ml-2 mt-3" data-toggle="toggle">
+        <i class="fe fe-x"><span class="sr-only"></span></i>
+    </a>
+    <nav class="vertnav navbar navbar-light">
+        <!-- nav bar -->
+        <div class="w-100 mb-4 d-flex">
+            <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="./index.html">
+                <svg version="1.1" id="logo" class="navbar-brand-img brand-sm" xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 120 120"
+                    xml:space="preserve">
+                    <g>
+                        <polygon class="st0" points="78,105 15,105 24,87 87,87 	" />
+                        <polygon class="st0" points="96,69 33,69 42,51 105,51 	" />
+                        <polygon class="st0" points="78,33 15,33 24,15 87,15 	" />
+                    </g>
+                </svg>
+            </a>
+        </div>
+        <ul class="navbar-nav flex-fill w-100 mb-2">
+            <li class="nav-item w-100">
+                <a class="nav-link" href="/index">
+                    <i class="fe fe-home fe-16"></i>
+                    <span class="ml-3 item-text">Dashboard</span>
+                </a>
+            </li>
+            <li class="nav-item w-100">
+                <a class="nav-link" href="/dataayam">
+                    <i class="fe fe-grid fe-16"></i>
+                    <span class="ml-3 item-text">Data Ayam</span>
+                </a>
+            </li>
+            <li class="nav-item w-100">
+                <a class="nav-link" href="/datapakan">
+                    <i class="fe fe-slack fe-16"></i>
+                    <span class="ml-3 item-text">Data Pakan</span>
+                </a>
+            </li>
+            <li class="nav-item w-100">
+                <a class="nav-link" href="/dataovk">
+                    <i class="fe fe-file-text fe-16"></i>
+                    <span class="ml-3 item-text">Data OVK</span>
+                </a>
+            </li>
+            <li class="nav-item w-100">
+                <a class="nav-link" href="/datatenagakerja">
+                    <i class="fe fe-user fe-16"></i>
+                    <span class="ml-3 item-text">Data Tenaga Kerja</span>
+                </a>
+            </li>
+            <li class="nav-item w-100">
+                <a class="nav-link" href="/datadistribusi">
+                    <i class="fe fe-share fe-16"></i>
+                    <span class="ml-3 item-text">Data Distribusi</span>
+                </a>
+            </li>
+            <li class="nav-item w-100">
+                <a class="nav-link" href="/datapengeluaran">
+                    <i class="fe fe-activity fe-16"></i>
+                    <span class="ml-3 item-text">Data Pengeluaran</span>
+                </a>
+            </li>
+            <li class="nav-item w-100">
+                <a class="nav-link" href="/datapendapatan">
+                    <i class="fe fe-bar-chart-2 fe-16"></i>
+                    <span class="ml-3 item-text">Data Pendapatan</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</aside>
