9f3fff0420b114c95bbf12975602fc741a94c9fc
apsarikaelysia <117898671+apsarikaelysia@users.noreply.github.com>
2023-06-06 22:56:02 +0700

    update api ssomething

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,48 @@
 <?php
 
+use App\Http\Controllers\ApiAuthController;
+use App\Http\Controllers\ApiDataAyamController;
+use App\Http\Controllers\ApiDataOvkController;
+use App\Http\Controllers\ApiDataPakanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+//login
+Route::post('login', [ApiAuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//data ayam
+Route::post('/data-ayam', [ApiDataAyamController::class, 'create']);
+Route::get('/data-ayam', [ApiDataAyamController::class, 'read']);
+Route::put('/data-ayam/{id}', [ApiDataAyamController::class, 'update']);
+Route::delete('/data-ayam/{id}', [ApiDataAyamController::class, 'delete']);
+
+//count jumlah ayam
+Route::get('data-ayam-counts', [ApiDataAyamController::class, 'getTotalCounts']);
+
+
+//data pakan
+Route::post('/data-pakan', [ApiDataPakanController::class, 'create']);
+Route::get('/data-pakan', [ApiDataPakanController::class, 'read']);
+Route::put('/data-pakan/{id}', [ApiDataPakanController::class, 'update']);
+Route::delete('/data-pakan/{id}', [ApiDataPakanController::class, 'delete']);
+
+//data pakan minggu ini
+Route::get('/data-pakan-weeks', [ApiDataPakanController::class, 'jumlahPakanMingguIni']);
+
+//data pakan bulan ini
+Route::get('/data-pakan-month', [ApiDataPakanController::class, 'jumlahPakanBulanIni']);
+
+//stok pakan
+Route::get('/data-pakan-stok', [ApiDataPakanController::class, 'stockPakan']);
+
+//data ovk
+Route::post('/data-ovk', [ApiDataOvkController::class, 'create']);
+Route::get('/data-ovk', [ApiDataOvkController::class, 'read']);
+Route::get('/data-ovk-last', [ApiDataOvkController::class, 'readIdTerakhir']);
+Route::put('/data-ovk/{id}', [ApiDataOvkController::class, 'update']);
+Route::delete('/data-ovk/{id}', [ApiDataOvkController::class, 'delete']);
+
+//logout
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [ApiAuthController::class, 'logout']);
 });
